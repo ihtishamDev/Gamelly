cbb9f6c4ee47334352f31b45f21d197ed5034634
ihtishamDev <ihtishamjhn@gmail.com>
2025-04-23 06:32:05 +0500

    Initial commit for Gamelly project

--- src/app/(pages)/Topbar/page.tsx
+++ src/app/(pages)/Topbar/page.tsx
@@ -12,13 +12,18 @@ const Topbar = () => {
     <div className="w-full shadow-sm bg-white">
       <div className="max-w-7xl mx-auto px-4 py-3 flex items-center justify-between">
         <div className="flex items-center gap-8">
+        <Link href="/Dashboard">
+
           <div className="flex items-center gap-2">
             <Image src="/pngwing.png" alt="logo" width={24} height={24} />
+            
             <span className="font-semibold text-lg">Gamelly</span>
+            
           </div>
+          </Link>
 
           <div className="hidden md:flex items-center gap-6 text-sm font-medium text-black">
-            <a href="#">Home</a>
+            <a href="/Dashboard">Home</a>
             <a href="#">Markets</a>
             <a href="#">Trade</a>
             <a href="/Account">My Account</a>
