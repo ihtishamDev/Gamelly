8eb4b13fcf1b4931fe3c30d600f86b5519a15ba0
ihtishamDev <ihtishamjhn@gmail.com>
2025-04-23 06:13:25 +0500

    adjust web icon

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,14 @@
 import Dashboard from "./(pages)/Dashboard/page"; // Assuming filename is page.tsx
+import Head from 'next/head';
 
 export default function Home() {
   return (
-    <div className="">
+    <div>
+      <Head>
+        <title>Gamelly</title> {/* Optional: Sets the tab title */}
+        <link rel="icon" href="/pngwing.png" /> {/* This sets the icon */}
+      </Head>
+
       <Dashboard />
     </div>
   );
